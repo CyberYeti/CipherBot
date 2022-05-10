0afb7b27f6ad4ac762df72f7f59f5bbea22c7cbc
CyberYeti <yitmengchin@gmail.com>
2022-05-10 11:06:01 -0700

    Initial Commit
    Contains the basic discord bot functionality and framework for command setup.
    Added the commands help and quote
    
    Gets a random quotes from the API
    https://api.quotable.io

---
+++ CipherBot.py
@@ -0,0 +1,84 @@
+import json, time, requests, threading
+import discord
+
+#region Get Token
+f = __file__.split("\\")
+f = f[:len(f)-1]
+directoryPath = "\\".join(f)
+
+with open(f'{directoryPath}\\Info.json', 'r') as f:
+    data = json.load(f)
+
+TOKEN = data['Token']
+#endregion
+
+#region Quote Getter
+def GetQuote():
+    #response = requests.get("https://zenquotes.io/api/random")
+    response = requests.get("https://api.quotable.io/random")
+    jsonData = json.loads(response.text)
+    quoteData = {'q': jsonData['content'], 'a': jsonData['author']}
+    return quoteData
+
+quotes = []
+minQuotes = 100
+def RefillQuotes():
+    while True:
+        if len(quotes) < 100:
+            quotes.append(GetQuote())
+        else:
+            time.sleep(1)
+
+quoteRefillThread = threading.Thread(target=RefillQuotes)
+quoteRefillThread.start()
+
+def NextQuote():
+    return quotes.pop()
+#endregion
+
+#region Cipher Methods
+#endregion
+
+#region Discord Bot Commands
+async def RandQuote(message):
+    quote = NextQuote()
+    await message.channel.send(f"{quote['q']} -{quote['a']}")
+
+async def HelpCommand(message):
+    text = "The available commands are:"
+    for key in CMDS:
+        text += f"\nc.{key}"
+    await message.channel.send(text)
+
+#endregion
+
+#region Discord Bot
+client = discord.Client()
+
+CMD_HEADER = "c."
+CMDS = {
+    "help": HelpCommand,
+    "quote": RandQuote,
+}
+
+@client.event
+async def on_ready():
+    print(f"{client.user} is now online")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user: return
+    
+    content = message.content
+    author = str(message.author)
+    channel = message.channel
+
+    if content.startswith(CMD_HEADER):
+        command = content[len(CMD_HEADER):]
+        if command in CMDS:
+            await CMDS[command](message)
+        else:
+            await channel.send(f"Unknown command called. Please type c.help for a list of commands.")
+
+client.run(TOKEN)
+#endregion
